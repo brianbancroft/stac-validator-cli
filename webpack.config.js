21064af8c17ebf1fe1f3c71eee847089851d85c5
Brian Bancroft <hello@brianbancroft.ca>
2019-04-15 18:22:38 -0400

    ⬆️  Update stac-validator package
    - Pass all fixes to this library

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,13 @@ module.exports = {
   entry: './src/index.js',
   target: 'node',
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: ['shebang-loader', 'babel-loader'],
+      },
+    ],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
