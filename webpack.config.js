b2d2da9949b7063e73755ab8f2ac1dc9a55661dc
Brian Bancroft <hello@brianbancroft.ca>
2019-04-15 13:36:02 -0400

    🔧  Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  target: 'node',
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+}
